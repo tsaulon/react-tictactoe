0607cbd8fdce2fcf0e2f005ab9c1684b51ce13cb
Tyrone Saulon <tyrone.saulon@hotmail.com>
2018-11-07 17:31:02 -0500

    Adding Winner calculations

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.props.value}  {/*  reference property value passed from Board.renderSquare(...);*/}
       </button>
     );
@@ -13,12 +14,31 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null), //  set default values.
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    const squaresCopy = this.state.squares.slice(); //  shallow copy current state of the board
+    squaresCopy[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squaresCopy,
+      xIsNext: !this.state.xIsNext
+    });  //  set new state of the board
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status = winner ? `Winner ${winner}` : `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
     return (
       <div>
@@ -65,3 +85,29 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+
+function calculateWinner(squares) {
+  const combos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  //  iterate through every combo and check if
+  for (let i = 0; i < combos.length; i++) {
+    const [a, b, c] = combos[i];  //  declare an array from the element of combo at index [i]
+
+    //  check if element at squares index of 'a' is 
+    //  check if the element at squares index of 'a' is the same as indexes 'b' and 'c'
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
